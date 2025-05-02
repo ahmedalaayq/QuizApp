db0cfd436c773dfe493fb5c80325c9b8f377ebe1
Ahmed Alaayq <ahmed.alaayq@gmail.com>
2025-05-02 16:49:01 +0300

    create app_assets file and generate assets , fix error in app_assets file and rename svg images

---
+++ lib/core/app_assets/app_assets.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_single_quotes
+class AppAssets {
+  AppAssets._();
+  
+  /// Assets for assetsImagesOnboardingOne
+  /// assets/images/onboarding_one.svg
+  static const String assetsImagesOnboardingOne = "assets/images/onboarding_one.svg";
+
+  /// Assets for assetsImagesOnboardingThree
+  /// assets/images/onboarding_three.svg
+  static const String assetsImagesOnboardingThree = "assets/images/onboarding_three.svg";
+
+  /// Assets for assetsImagesOnboardingTwo
+  /// assets/images/onboarding_two.svg
+  static const String assetsImagesOnboardingTwo = "assets/images/onboarding_two.svg";
+}
+
